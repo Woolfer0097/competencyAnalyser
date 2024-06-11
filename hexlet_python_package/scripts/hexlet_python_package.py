92af42d982ac0dfd6b2e1343658e383208a5b0d4
Kochegarov Danila <60360761+Woolfer0097@users.noreply.github.com>
2024-06-11 21:16:37 +0300

    Initial commit

---
+++ hexlet_python_package/scripts/hexlet_python_package.py
@@ -0,0 +1,11 @@
+from hexlet_python_package import user
+
+
+def main():
+    """Run an example code."""
+    # it is ok to have some magical numbers locally
+    print(user.User(name='Bob', age=42).get_introduction())  # noqa:WPS432
+
+
+if __name__ == '__main__':
+    main()
